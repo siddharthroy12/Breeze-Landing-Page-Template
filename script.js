534ff70ed5a752542ec2a9982b623c02ef7b31ce
Siddharth Roy <siddharthroy36912@gmail.com>
2022-07-22 17:58:04 +0530

    Added more sections and some animations

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 window.addEventListener('load', function() {
-  console.log('Hello World');
+  const elementsToAnimate = document.querySelectorAll(".feature-box");
+
+  const elementInView = (el, percentageScroll = 100) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    return (
+      elementTop <=
+      ((window.innerHeight || document.documentElement.clientHeight) * (percentageScroll/100))
+    );
+  };
+
+  const animateElement = (element) => {
+    element.classList.add("fade-in");
+  };
+
+  const removeAnimationElement = (element) => {
+    element.classList.remove("fade-in");
+  };
+
+  const handleScrollAnimation = () => {
+    elementsToAnimate.forEach((el) => {
+      if (elementInView(el, 100)) {
+        animateElement(el);
+      } else {
+        removeAnimationElement(el);
+      }
+    })
+  };
+
+  window.addEventListener('scroll', () => {
+    handleScrollAnimation();
+  });
 })
